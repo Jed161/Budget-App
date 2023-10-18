2151e84c68d9924c06b82fb0004955e7ed0170c2
James Edmond <jamesedmond@pursuit.org>
2023-10-18 18:32:51 -0400

    First commit

---
+++ models/logs.js
@@ -0,0 +1,26 @@
+module.exports = [
+    {
+        id: "1",
+        itemName: "Credit Card",
+        amount: "50",
+        date: "10/15/23",
+        from: "Capital One",
+        category: "Bill",
+    },
+    {
+        id: "2",
+        itemName: "Credit Card",
+        amount: "40",
+        date: "10/12/23",
+        from: "Aspire",
+        category: "Bill",
+    },
+    {
+        id: "3",
+        itemName: "Credit Card",
+        amount: "30",
+        date: "10/1/23",
+        from: "Surge",
+        category: "Bill",
+    },
+];
